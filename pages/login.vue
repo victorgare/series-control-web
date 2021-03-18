82aaf1bf14f3924035e9be5a17b4a55cacb15cd5
Victor Rodrigues <garevictor@hotmail.com>
2021-03-18 19:23:33 -0300

    Melhorado erro no login

--- pages/login.vue
+++ pages/login.vue
@@ -76,7 +76,7 @@ export default {
             } catch (err) {
                 this.$modalAlert.showError({
                     title: 'Erro',
-                    text: `Problemas ao logar<br>${err}`,
+                    text: `Problemas ao logar<br>${JSON.stringify(err)}`,
                     okOnly: true,
                 })
             }
